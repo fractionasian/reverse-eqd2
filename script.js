0c2cf20ab401e944ae44e1f8cff748233c7d0633
Peter B. Nguyen <18570985+fractionasian@users.noreply.github.com>
2024-06-17 11:54:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ window.onload = calculateAllResults;
 
 // Register service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
         .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         })
